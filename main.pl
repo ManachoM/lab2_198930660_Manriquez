f8f102193de1bb1d288c975a0a815321912b9278
Manuel Ignacio Manríquez <manuel.manriquez.b@usach.cl>
2019-10-09 23:23:00 -0300

    Modificacion TDAs, simplificacion para permitir mejor implementacion

--- main.pl
+++ main.pl
@@ -1,5 +1,9 @@
-%Representaciones%
-%Escenario:  [N; M; Estado; Ptje; [Eq1]; [Eq2]]
-%Equipo: [Flag; [Lista de Personajes]]
+%Representaciones
+%Escenario:  [N; M; Estado; [Listas de personajes del jugador]; [Lista de personajes del computador]; [Par ordenado que da trayectoria de proyectil]]
 %Personaje: [X; Y; Vida]
+%Dominios:
+    %N, M: entero positivo
+    %Estado: string que describe estado del juego
+    %Lista de personajes del jugador: lista que contiene datos tipo personaje que pertenecen al equipo del jugador
+    %Lista de personajes del computador: lista que contiene datos tipo personaje que pertenecen al equipo del computador
 
