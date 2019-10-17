e9b8c9e51c814a10a79ab47e744ca2bd8e03f2ad
Manuel Ignacio Manríquez <manuel.manriquez.b@usach.cl>
2019-10-17 15:24:06 -0300

    Se agregan comentarios y encabezados de documentacion

--- main.pl
+++ main.pl
@@ -1,52 +1,48 @@
-%Representaciones
-%Escenario:  [ID; N; M; [Listas de personajes del jugador]; [Lista de personajes del computador]; [Par ordenado que da trayectoria de proyectil]; Dificultad; State; Score; [Lista con todo lo anterior]]
-%Personaje: [X; Y; Vida]
-%Dominios:
-    %ID: Entero que representa al tablero
-    %N, M: entero positivo, filas y columnas, respectivamente, del tablero en cuestion
-    %Lista de personajes del jugador: lista que contiene datos tipo personaje que pertenecen al equipo del jugador
-    %Lista de personajes del computador: lista que contiene datos tipo personaje que pertenecen al equipo del computador
-    %Par que contiene coordenadas de proyectil, de existir uno.
-
-escenario(Id, N, M, Eq1, Eq2, Pr, D, State, Score, Scene):- Scene=[Id, N, M, Eq1, Eq2, Pr, D, State, Score].
-getIdEscena(Scene, Id):- nth0(0, Scene, Id).
-getNEscenario(Scene, N):-nth0(1, Scene, N).
-getMEscenario(Scene, M):-nth0(2, Scene, M).
-getEq1Escenario(Scene, Eq1):-nth0(3, Scene, Eq1).
-getEq2Escenario(Scene, Eq2):- nth0(4, Scene, Eq2).
-getPrEscenario(Scene, Pr):- nth0(5, Scene, Pr).
-getDEscenario(Scene, D):- nth0(6, Scene, D).
-getStateEscenario(Scene, State):- nth0(7, Scene, State).
-getScoreEscenario(Scene, Score):- nth0(8, Scene, Score).
-
-%TDA: Proyectil
-%Representacion: [Posicion X inicial, Posicion Y inicial, Tiempo, Angle]
-%Dominios: Todos los elementos son enteros positivos
-proyectil(X,Y,T,Angle, P):-T>=0, Angle>=0, P=[X,Y,T,Angle].
-getXPr(P, X):- nth0(0, P, X).
-getYPr(P, Y):- nth0(1, P, Y).
-getTPr(P, T):- nth0(2, P, T).
-getAnglePr(P, Angle):- nth0(3, P, Angle).
-setXPr(Pin, NewX, Pout):- getYPr(Pin, Y), getTPr(Pin, T), getAnglePr(Pin, Angle), proyectil(NewX, Y, T, Angle, Pout).
-setYPr(Pin, NewY, Pout):- getXPr(Pin, X), getTPr(Pin, T), getAnglePr(Pin, Angle), proyectil(X, NewY, T, Angle, Pout).
-setTPr(Pin, NewT, Pout):- getXPr(Pin, X), getYPr(Pin, Y), getAnglePr(Pin, Angle), proyectil(X, Y, NewT, Angle, Pout).
-updatePr(Pin, T, Pos):-getXPr(Pin, X), getYPr(Pin, Y),getAnglePr(Pin, Angle), NewX is round(X+(6*cos(Angle)*T)), NewY is round(Y+(6*sin(Angle)*T - 2.5*T*T)), Pos=[NewX, NewY].
-initPr(Personaje, Angle,Pout):- getXPersonaje(Personaje, X), getYPersonaje(Personaje, Y), Angle>0, proyectil(X, Y, 0, Angle, Pout).
+/*
+LABORAORIO 2 - PARADIGMAS DE PROGRAMACION
+RUT: 19.893.066-0
+Manuel Ignacio Manriquez
+Este documento contiene el desarrollo del laboratorio 2 de la asignatura Paradigmas de Programacion
 
 
-%TDA: Personaje
-%Representacion: [Posicion X, Posicion Y, Vida]
-%Dominios: X, Y y Vida son todos enteros positivos
-personaje(X, Y, V, Personaje):- X>0, Y>0, V > -1, Personaje = [X,Y,V].
-esPersonaje(Personaje):- nth0(0, Personaje, X), nth0(1, Personaje, Y), nth0(2, Personaje, V), X > 0, Y > 0, V > -1.
-getXPersonaje(Personaje, X):- nth0(0, Personaje, X).
-getYPersonaje(Personaje, Y):- nth0(1, Personaje, Y).
-getVidaPersonaje(Personaje, V):- nth0(2, Personaje, V).
-setXPersonaje(NewX, Personaje, NewPersonaje):- NewX>0, getYPersonaje(Personaje, Y), getVidaPersonaje(Personaje, V), personaje(NewX, Y, V, NewPersonaje).
-setYPersonaje(NewY, Personaje, NewPersonaje):- NewY>0, getXPersonaje(Personaje, X), getVidaPersonaje(Personaje, V), personaje(X, NewY, V, NewPersonaje).
-setVidaPersonaje(NewV, Personaje, NewPersonaje):- NewV > -1, getXPersonaje(Personaje, X), getYPersonaje(Personaje, Y), personaje(X, Y, NewV, NewPersonaje).
+Dominios: 
+    Id, N, M, D, Angle, X, Y, Vida, Score, T, Member, MoveDir, Seed: Numeros positivos
+    State: string
+    Eq1, Eq2: lista con personajes
+    Pr: proyectil segun su representacion
+    Pos, PosCaida: par de numeros, representan posicion X,Y
+    
+    Hay ciertos elementos de dominio que estan representados por su estructura, por lo cual se explicita en la seccion de predicados que es exactamente lo que entra a cada predicado
+
+Predicados: 
+    escenario, personaje, proyectil ---> Son TDA, se explicitan mas adelante
+    compararListas(Lista 1, Lista 2)
+    generarX(Lista de personajes, lista con posiciones X)
+    generarPos(lista de personajes, lista con posiciones [X,Y])
+    replace(elemento a reemplazar, elemento nuevo, lista antigua, lista con el elemento reemplazado)
+    esMiembro(elemento del cual se estudia pertenencia, lista en la cual se estudia)
+    seRepite(lista de elementos en la que se estudia la repeticion de elementos)
+    largoLista(lista en cuestion, valor de su largo)
+    posCaida(proyectil que se estudia, posicion en la cual intersecta con el suelo de la forma [X,Y])
+    quedanVivos(lista con personajes)
+    cuantosMuertos(lista con personajes, numero de personajes con vida igual a cero)
+    state(lista con personajes del jugador, lista con personajes del PC, string que contiene el estado de la partida)
+    score(lista con personajes del jugador, lista con personajes del PC, entero que representa el puntaje de la partida)
+    quienDispara(lista con personajes, lista con estructura de personaje)
+    shootBack(escenario inicial, escenario actualizado tras disparo del PC)
+    createScene(cantidad de filas, cantida de columnas, cantidad de enemigos, dificultad, semilla, escenario en su representacion)
+    checkScene(representacion de escenario)
+    moveMember(escena inicial, posicion de personaje a mover en lista de equipo (parte de 0), cantidad a mover, semilla, escenario de salida)
+    shoot(escenario de entrada, posicion del personaje que realiza disparo, tipo de disparo (es irrelevante, hay un solo tipo), angulo de disparo en radianes, semilla, escenario de salida)
+    updateScene(escenario de entrada, semilla, escenario actualizado a t+1)
+    play(escenario de entrada, posicion en lista de personaje que realizara accion, cantidad y sentido de movimiento, tipo de disparom (irrelevante), angulo en radianes, semilla, escenario de salida)
+
+METAS
+    Primarias o Principales: createScene, checkScene, moveMember, shoot, updateScene, play
+    Secundarias: TDA y predicados asociados, escenario, compararListas, generarX, generarPos, replace, esMiembro, seRepite, largoLista, posCaida, quedanVivos, cuantosMuertos, state, score, quienDispara, shootBack
 
 
+*/
 %CLAUSLAS
 
 
@@ -60,8 +56,73 @@ escenario(5, 10, 12, [[1, 7, 1], [2, 7, 1], [4, 7, 1]], [[5, 7, 1], [6, 7, 1], [
 escenario(6, 20, 20, [[2, 12, 1], [5, 12, 1], [7, 12, 1]], [[10, 12, 1], [12, 12, 1], [13, 12, 1], [14, 12, 1], [15, 12, 1], [17, 12 , 1], [18, 12 , 1], [19, 12, 1]], [], 3, "PLAYING", 0,[6, 20, 20, [[2, 12, 1], [5, 12, 1], [7, 12, 1]], [[10, 12, 1], [12, 12, 1], [13, 12, 1], [14, 12, 1], [15, 12, 1], [17, 12 , 1], [18, 12 , 1], [19, 12, 1]], [], 3, "PLAYING", 0]).
 
 
-%Reglas con sus respectivos casos bases
 
+%TDA: Escenario
+%Representacion:  [ID; N; M; [Listas de personajes del jugador]; [Lista de personajes del computador]; [Par ordenado que da trayectoria de proyectil]; Dificultad; State; Score;]
+%Personaje: [X; Y; Vida]
+%Dominios:
+    %ID: Entero que representa al tablero
+    %N, M: entero positivo, filas y columnas, respectivamente, del tablero en cuestion
+    %Lista de personajes del jugador: lista que contiene datos tipo personaje que pertenecen al equipo del jugador
+    %Lista de personajes del computador: lista que contiene datos tipo personaje que pertenecen al equipo del computador
+    %Par que contiene coordenadas de proyectil, de existir uno.
+    %Dificultad: entero que representa la dificultad
+    %State: string que refleja el estado del escenario
+    %Score: entero que muestra quien va ganando
+%Predicado constructor
+escenario(Id, N, M, Eq1, Eq2, Pr, D, State, Score, Scene):- Scene=[Id, N, M, Eq1, Eq2, Pr, D, State, Score].
+%Se considera predicado de pertenencia checkScene, es requerimiento funcional por lo que se encuentra mas abajo
+%Predicados selectores
+getIdEscena(Scene, Id):- nth0(0, Scene, Id).
+getNEscenario(Scene, N):-nth0(1, Scene, N).
+getMEscenario(Scene, M):-nth0(2, Scene, M).
+getEq1Escenario(Scene, Eq1):-nth0(3, Scene, Eq1).
+getEq2Escenario(Scene, Eq2):- nth0(4, Scene, Eq2).
+getPrEscenario(Scene, Pr):- nth0(5, Scene, Pr).
+getDEscenario(Scene, D):- nth0(6, Scene, D).
+getStateEscenario(Scene, State):- nth0(7, Scene, State).
+getScoreEscenario(Scene, Score):- nth0(8, Scene, Score).
+
+%TDA: Proyectil
+%Representacion: [Posicion X inicial, Posicion Y inicial, Tiempo, Angle]
+%Dominios: Todos los elementos son enteros positivos
+%Predicado constructor
+proyectil(X,Y,T,Angle, P):-T>=0, Angle>=0, P=[X,Y,T,Angle].
+%Predicados selectores
+getXPr(P, X):- nth0(0, P, X).
+getYPr(P, Y):- nth0(1, P, Y).
+getTPr(P, T):- nth0(2, P, T).
+getAnglePr(P, Angle):- nth0(3, P, Angle).
+%Predicados modificadores
+setXPr(Pin, NewX, Pout):- getYPr(Pin, Y), getTPr(Pin, T), getAnglePr(Pin, Angle), proyectil(NewX, Y, T, Angle, Pout).
+setYPr(Pin, NewY, Pout):- getXPr(Pin, X), getTPr(Pin, T), getAnglePr(Pin, Angle), proyectil(X, NewY, T, Angle, Pout).
+setTPr(Pin, NewT, Pout):- getXPr(Pin, X), getYPr(Pin, Y), getAnglePr(Pin, Angle), proyectil(X, Y, NewT, Angle, Pout).
+%Otros predicados, uno que inicializa proyectil y otro que calcula su posicion en un tiempo T.
+updatePr(Pin, T, Pos):-getXPr(Pin, X), getYPr(Pin, Y),getAnglePr(Pin, Angle), NewX is round(X+(6*cos(Angle)*T)), NewY is round(Y+(6*sin(Angle)*T - 2.5*T*T)), Pos=[NewX, NewY].
+initPr(Personaje, Angle,Pout):- getXPersonaje(Personaje, X), getYPersonaje(Personaje, Y), Angle>0, proyectil(X, Y, 0, Angle, Pout).
+
+
+%TDA: Personaje
+%Representacion: [Posicion X, Posicion Y, Vida]
+%Dominios: X, Y y Vida son todos enteros positivos
+%Predicado constructor
+personaje(X, Y, V, Personaje):- X>0, Y>0, V > -1, Personaje = [X,Y,V].
+%Predicado de pertenencia
+esPersonaje(Personaje):- nth0(0, Personaje, X), nth0(1, Personaje, Y), nth0(2, Personaje, V), X > 0, Y > 0, V > -1.
+%Predicados selectores
+getXPersonaje(Personaje, X):- nth0(0, Personaje, X).
+getYPersonaje(Personaje, Y):- nth0(1, Personaje, Y).
+getVidaPersonaje(Personaje, V):- nth0(2, Personaje, V).
+%Predicados modificadores
+setXPersonaje(NewX, Personaje, NewPersonaje):- NewX>0, getYPersonaje(Personaje, Y), getVidaPersonaje(Personaje, V), personaje(NewX, Y, V, NewPersonaje).
+setYPersonaje(NewY, Personaje, NewPersonaje):- NewY>0, getXPersonaje(Personaje, X), getVidaPersonaje(Personaje, V), personaje(X, NewY, V, NewPersonaje).
+setVidaPersonaje(NewV, Personaje, NewPersonaje):- NewV > -1, getXPersonaje(Personaje, X), getYPersonaje(Personaje, Y), personaje(X, Y, NewV, NewPersonaje).
+
+
+
+
+%Reglas con sus respectivos casos bases
+%Reglas auxiliares, o metas secundarias
 compararListas([A], [B]):- A=B.
 compararListas([X|Xs], [Y|Ys]):- X=Y, compararListas(Xs, Ys). 
 
@@ -70,6 +131,7 @@ generarX([[X,_,1]|Resto], [X|Y]):-generarX(Resto, Y).
 
 generarPos([[X, Y, 1]], [[X,Y]]).
 generarPos([[X,Y,1]|Cola], [[X,Y]|Resto]):- generarPos(Cola, Resto).
+generarPos([[_,_,0]|Cola], Pos):- generarPos(Cola, Pos).
 
 replace(_, _, [], []).
 replace(O, R, [O|T], [R|T2]) :- replace(O, R, T, T2).
@@ -113,7 +175,28 @@ quienDispara([[_,_,0]], Pos):- Pos = [0,0,0].
 quienDispara([[X,Y,1]|_], Pos):- Pos = [X,Y,1].
 quienDispara([[_,_,0]|Cola], Pos):- quienDispara(Cola, Pos).
 
+shootBack(SceneIn, SceneOut):-
+    getNEscenario(SceneIn, N),
+    getMEscenario(SceneIn, M),
+    getEq1Escenario(SceneIn, Eq1),
+    getEq2Escenario(SceneIn, Eq2),
+    getDEscenario(SceneIn, D),
+    quienDispara(Eq2, Prje),
+    random(1.5707963, 3.1415926, Angle),
+    initPr(Prje, Angle, Pr),
+    generarPos(Eq1, X1),
+    posCaida(Pr, Pos),
+    member(Pos, X1),
+    nth0(0, Pos, X),
+    nth0(1, Pos, Y),
+    replace([X,Y,1], [X,Y,0], Eq1, NewEq1),
+    state(NewEq1, Eq2, State),
+    score(NewEq1, Eq2, Score),
+    escenario(0,N,M,NewEq1,Eq2,[],D,State,Score,SceneOut);
+    SceneOut=SceneIn.
+    
 
+%Reglas principales o Metas Primarias
 createScene(N, M, E, D, Seed, Scene):- 
     random(1, Seed, ID),
     escenario(ID, N, M, _, Eq2, _, D, _, _, Scene), 
@@ -122,10 +205,10 @@ createScene(N, M, E, D, Seed, Scene):-
 checkScene(Scene):- 
      escenario(_,_,_,_,_,_,_,_,_,Scene) ;
      (
-      nth0(1, Scene, N),
-      nth0(2, Scene, M),
-      nth0(3, Scene, Eq1), 
-      nth0(4, Scene, Eq2),
+      getNEscenario(Scene, N),
+      getMEscenario(Scene, M),
+      getEq1Escenario(Scene, Eq1), 
+      getEq2Escenario(Scene, Eq2),
       N>0, 
       M>0, 
       length(Eq1, L1), 
@@ -153,13 +236,15 @@ moveMember(SceneIn, Member, MoveDir, _, SceneOut):-
     getXPersonaje(Prje, X),
     NewX is MoveDir+X,
     NewX>0,
+    NewX =< M,
     setXPersonaje(NewX, Prje, NewPrje),
     replace(Prje, NewPrje, Eq1, NewEq),
     generarX(Eq1, X1),
     generarX(Eq2, X2),
     append(X1,X2,L),
     not(member(NewX, L)),
-    escenario(0, N, M, NewEq, Eq2, P, D, Ste, Score, SceneOut).
+    escenario(0, N, M, NewEq, Eq2, P, D, Ste, Score, SceneOut);
+    SceneOut=SceneIn.
 
 
 
@@ -182,26 +267,6 @@ shoot(SceneIn, Member, _, Angle, _, SceneOut):-
     escenario(0, N, M, Eq1, NewEq2, [], D, State, Score,SceneOut);
     SceneOut = SceneIn.
 
-shootBack(SceneIn, SceneOut):-
-    getNEscenario(SceneIn, N),
-    getMEscenario(SceneIn, M),
-    getEq1Escenario(SceneIn, Eq1),
-    getEq2Escenario(SceneIn, Eq2),
-    getDEscenario(SceneIn, D),
-    quienDispara(Eq2, Prje),
-    random(1.5707963, 3.1415926, Angle),
-    initPr(Prje, Angle, Pr),
-    generarPos(Eq1, X1),
-    posCaida(Pr, Pos),
-    member(Pos, X1),
-    nth0(0, Pos, X),
-    nth0(1, Pos, Y),
-    replace([X,Y,1], [X,Y,0], Eq1, NewEq1),
-    state(NewEq1, Eq2, State),
-    score(NewEq1, Eq2, Score),
-    escenario(0,N,M,NewEq1,Eq2,[],D,State,Score,SceneOut);
-    SceneOut=SceneIn.
-    
 
 
 updateScene(SceneIn, _, SceneOut):- 
