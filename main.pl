ad0f1631641825a38b4952fa35849158f75264db
Manuel Ignacio Manríquez <manuel.manriquez.b@usach.cl>
2019-10-17 22:50:24 -0300

    Se agregan ultimos comentarios

--- main.pl
+++ main.pl
@@ -47,7 +47,7 @@ METAS
 
 
 %Base de conocimientos
-%escenario(Id, N, M, Eq1, Eq2, P, [Lista con representacion]).
+%escenario(Id, N, M, Eq1, Eq2, P, D, State, Score, [Lista con representacion]).
 escenario(1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0,[1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0]).
 escenario(2, 5, 10, [[2, 4, 1], [3, 4, 1], [4, 4, 1]], [[7, 4, 1], [8, 4, 1], [9, 4, 1], [10, 4, 1]], [], 2, "PLAYING", 0,[2, 5, 10, [[2, 4, 1], [3, 4, 1], [4, 4, 1]], [[7, 4, 1], [8, 4, 1], [9, 4, 1], [10, 4, 1]], [], 2, "PLAYING", 0]).
 escenario(3, 5, 10, [[1, 1, 1], [3, 1, 1], [5, 1, 1]], [[6, 1, 1], [7, 1, 1], [8, 1, 1], [9, 1, 1], [10, 1, 1]], [], 3, "PLAYING", 0,[3, 5, 10, [[1, 1, 1], [3, 1, 1], [5, 1, 1]], [[6, 1, 1], [7, 1, 1], [8, 1, 1], [9, 1, 1], [10, 1, 1]], [], 3, "PLAYING", 0]).
@@ -123,39 +123,99 @@ setVidaPersonaje(NewV, Personaje, NewPersonaje):- NewV > -1, getXPersonaje(Perso
 
 %Reglas con sus respectivos casos bases
 %Reglas auxiliares, o metas secundarias
+
+/*
+Predicado que recibe dos listas y compara que sus elementos sean iguales
+Entrada: lista X lista
+Salida: boolean
+*/
 compararListas([A], [B]):- A=B.
 compararListas([X|Xs], [Y|Ys]):- X=Y, compararListas(Xs, Ys). 
 
+
+/*
+Predicado que genera lista con posiciones X de lista de personajes
+Entrada: lista de personajes X lista de enteros (usar de salida)
+Salida: lista de enteros
+*/
 generarX([[X, _, 1]], [X]).
 generarX([[X,_,1]|Resto], [X|Y]):-generarX(Resto, Y).
 
+/*
+Similar a predicado anterior, devuelve lista de pares
+Entrada: lista de personajes X lista de pares(salida)
+Salida: lista de pares
+*/
 generarPos([[X, Y, 1]], [[X,Y]]).
 generarPos([[X,Y,1]|Cola], [[X,Y]|Resto]):- generarPos(Cola, Resto).
 generarPos([[_,_,0]|Cola], Pos):- generarPos(Cola, Pos).
 
+/*
+Predicado que permite reemplazar un elemento en una lista
+Entrada: elemento X elemento X lista antigua X lista nueva(salida)
+Salida: lista con elemento reemplazado
+*/
 replace(_, _, [], []).
 replace(O, R, [O|T], [R|T2]) :- replace(O, R, T, T2).
 replace(O, R, [H|T], [H|T2]) :- H \= O, replace(O, R, T, T2).
 
+/*
+Predicado que permite saber si un elemento es miembro de una lista
+Entrada: elemento X lista de elementos(salida)
+Salida: boolean
+*/
 esMiembro(M, [M|_]).
 esMiembro(M, [M]).
 esMiembro(M, [L|Ls]):- esMiembro(M, Ls) ; M=L.
 
+
+/*
+Predicado que permite saber si se repite un elemento en una lista
+Entrada: lista
+Salida: bollean
+*/
 seRepite([M, M]).
 seRepite([L|Ls]):- esMiembro(L, Ls) ; seRepite(Ls).
 
+
+/*
+Predicado que permite saber largo de una lista
+Entrada: lista X entero (salida)
+Salida: entero, cantidad de elementos de una lista
+*/
 largoLista([], 0).
 largoLista([_|Xs], N):- largoLista(Xs, N1), N is N1+1.
 
+/*
+Predicado que calcula la posicion de llegada de un proyectil
+Entrada: proyectil X par de posicion (salida)
+Salida: par con posicion de llegada
+*/
 posCaida(Pr, Pos):- getAnglePr(Pr, Angle), Tmax is (2.4*sin(Angle)), updatePr(Pr, Tmax, Pos).
 
+/*
+Predicado que permite saber si quedan personajes vivos en una lista
+Entrada: lista de personajes
+Salida: boolean
+*/
 quedanVivos([[_,_,1]]).
 quedanVivos([[_,_,1]|_]).
 quedanVivos([_|Cs]):- quedanVivos(Cs).
 
+/*
+Predicado que entrega cuantos personajes muertos hay en un equipo
+Entrada: lista con personajes X entero (salida)
+Salida: entero, cantidad de muertos
+*/
 cuantosMuertos([Personaje], Res):- getVidaPersonaje(Personaje, R), R is 0, Res is 1; Res is 0.
 cuantosMuertos([Cabeza|Cola], Res):- cuantosMuertos([Cabeza], R1), cuantosMuertos(Cola, R2), Res is R1+R2.
 
+
+/*
+Predicado que determina estado de juego a partir de dos equipos
+Entrada: lista de personajes X lista de personajes X string (salida)
+Salida: string que refleja estado de juego
+*/
 state(Eq1, Eq2, State):- 
     quedanVivos(Eq1),
     quedanVivos(Eq2),
@@ -168,13 +228,29 @@ state(Eq1, Eq2, State):-
     State="DEFEAT";
     State="DRAW".
 
+/*
+Predicado que determina puntaje de partida a partir de dos equipos
+Entrada: lista de personajes X lista de personajes X entero (salida)
+Salida: puntaje de escenario
+*/
 score(Eq1, Eq2, Score):- cuantosMuertos(Eq2, S1), cuantosMuertos(Eq1, S2), Score is S1-S2.
 
+
+/*
+Predicado que permite saber que personaje de una lista esta en condiciones de ejecutar un disparo
+Entrada: lista de personajes X personaje (salida)
+Salida: personaje capacitado
+*/
 quienDispara([[A,B,1]], Pos):- Pos = [A,B,1].
 quienDispara([[_,_,0]], Pos):- Pos = [0,0,0].
 quienDispara([[X,Y,1]|_], Pos):- Pos = [X,Y,1].
 quienDispara([[_,_,0]|Cola], Pos):- quienDispara(Cola, Pos).
 
+/*
+Predicado analogo a shoot, solo que actua sobre equipo de jugador
+Entrada: escenario X escenario(salida)
+Salida: escenario despues de que equipo enemigo realiza un disparo.
+*/
 shootBack(SceneIn, SceneOut):-
     getNEscenario(SceneIn, N),
     getMEscenario(SceneIn, M),
@@ -197,14 +273,36 @@ shootBack(SceneIn, SceneOut):-
     
 
 %Reglas principales o Metas Primarias
+
+/*
+Predicado que consulta si es posible crear un escenario con dimensiones NxM, cantidad de enemigos E, 
+dificultad D y parametro aleatorio Seed.
+Entrada: entero X entero X entero X entero X escenario(usar de salida)
+Salida: dejar SceneOut como salida. De ser posible, retorna un escenario armado, sino, una estructura con direcciones de memoria
+*/
 createScene(N, M, E, D, Seed, Scene):- 
-    random(1, Seed, ID),
-    escenario(ID, N, M, _, Eq2, _, D, _, _, Scene), 
+    random_between(1, Seed, ID),
     E = L, 
-    largoLista(Eq2, L).
+    largoLista(Eq2, L),
+    escenario(ID, N, M, _, Eq2, [], D, "PLAYING", 0, Scene).
+
+
+/*
+Predicado que permite verificar si un elemento constituye un escenario valido
+Entrada: cualquier cosa, se obtiene el maximo provecho de este predicado al ingresar una representacion de escenario
+Salida: booleano
+*/
 checkScene(Scene):- 
-     escenario(_,_,_,_,_,_,_,_,_,Scene) ;
-     (
+      escenario(_,N,M,Eq1,Eq2,_,_,_,_,Scene),
+      N>0, 
+      M>0, 
+      length(Eq1, L1), 
+      length(Eq2, L2), 
+      M>L1+L2,
+      generarX(Eq1, X1), 
+      generarX(Eq2, X2),
+      append(X1, X2, L),  
+      not(seRepite(L)) ;
       getNEscenario(Scene, N),
       getMEscenario(Scene, M),
       getEq1Escenario(Scene, Eq1), 
@@ -214,15 +312,17 @@ checkScene(Scene):-
       length(Eq1, L1), 
       length(Eq2, L2), 
       M>L1+L2,
-      append(X1, X2, L), 
       generarX(Eq1, X1), 
       generarX(Eq2, X2), 
-      not(seRepite(L))
-      ).
+      append(X1, X2, L), 
+      not(seRepite(L)).
 
 
-
-
+/*
+Predicado que permite mover al miembro Member del equipo en una direccion MoveDir en un SceneOut
+Entrada: escenario X entero X  entero X cualquier cosa X escenario (usar de salida)
+Salida: escenario, modificado de ser validos los parametros, o la entrada si no lo son
+*/
 moveMember(SceneIn, Member, MoveDir, _, SceneOut):- 
     getNEscenario(SceneIn, N),
     getMEscenario(SceneIn, M),
@@ -247,7 +347,12 @@ moveMember(SceneIn, Member, MoveDir, _, SceneOut):-
     SceneOut=SceneIn.
 
 
-
+/*
+Predicado que realiza un disparo, intersecta con equipo enemigo, modifica a personaje de ser necesario,
+modifica estado y puntaje
+Entrada: escenario X entero X cualquier cosa X numero (rad) X cualquier cosa X escenario (usar de salida)
+Salida: escenario, modificado si los parametros son validos, o el mismo si no lo son
+*/
 shoot(SceneIn, Member, _, Angle, _, SceneOut):-
     getNEscenario(SceneIn, N),
     getMEscenario(SceneIn, M),
@@ -268,7 +373,11 @@ shoot(SceneIn, Member, _, Angle, _, SceneOut):-
     SceneOut = SceneIn.
 
 
-
+/*
+Predicado que permite actualizar el esenario y sus componentes de un timepo t a t+1
+Entrada: escenario X cualquier cosa X escenario (usar de salida)
+Salida: escenario, modificado de ser valido el tiempo de evaluacion, el mismo si el tiempo es demasiado grande (o sea, ya no choco con nada)
+*/
 updateScene(SceneIn, _, SceneOut):- 
     getNEscenario(SceneIn, N),
     getMEscenario(SceneIn, M),
@@ -309,8 +418,60 @@ updateScene(SceneIn, _, SceneOut):-
     escenario(0, N, M, Eq1, Eq2, NewPr, D, State, Score, SceneOut);
     SceneOut = SceneIn.
 
-
+/*
+Predicado que permite realizar un turno completo, llamese, mover personaje, disparar, actualizar equipo enemigo, rexibir disparo de vuelta y modificar estado y puntaje
+Entrada: escenario X entero X entero X cualquier cosa X numero (rad) X cualquier cosa X escenario (usar de salida)
+Salida: escenario, modificado en caso de pegarle a alguien o ser golpeado de vuelta, o el mismo, si es que parametros no son validos
+*/
 play(SceneIn, Member, MoveDir, _, Angle, _, SceneOut):- 
     moveMember(SceneIn, Member, MoveDir, 12, Scene1),
     shoot(Scene1, Member, 123123, Angle, 123, Scene2),
     shootBack(Scene2, SceneOut).
+
+
+
+
+
+
+
+/*
+Aqui procederemos a incluir los ejemplos de uso para predicados / metas principales
+
+-------createScene-------
+createScene(5, 10, 2, 1, 1, SceneOut).
+createScene(5, 10, 4, 2, 2, SceneOut).
+createScene(20, 20, 8, 3, 6, SceneOut). --> Consultar varias veces, debido al elemento aleatorio de Seed, a veces crea un escenario no presente en base de conocimientos
+
+
+-------checkScene--------
+checkScene([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0]).
+checkScene([1, 6, 114, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0]).---> Ambos son verdaderos, aunque uno se encuentra en base de conocimientos y otro no.
+checkScene([1, 6, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [3, 2, 1]], [], 1, "PLAYING", 0]). ---> Retorna falso porque se repite una posicion X
+
+
+-------moveMember--------
+moveMember([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0], 2, 3, 312, SceneOut). ---> Funciona y mueve al tercer integrante de la lista tres espacios hacia la derecha
+moveMember([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0], 2, -1, 312, SceneOut). ---> Retorna el escenario inicial, ya que es imposible moverse y quedarse un espacio hacia la derecha
+moveMember([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0], 0, -1, 312, SceneOut). ----> Retorna el escenario inicial, ya que el primer personaje se saldria del tablero si se mueve un espacio a la izquierda
+
+
+-------shoot--------
+shoot([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0], 2, 123, 0.5, 123, SceneOut). ---> Retorna escenario nuevo en el cual se modifica vida de un personaje enemigo y cambia el puntaje 1. Notar que tambien cambia Id a 0
+shoot([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0], 2, 123, 0, 123, SceneOut). ----> Retorna el mismo escenario, ya que no le acierta a ningun personaje enemigo
+shoot([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 0], [9, 2, 1]], [], 1, "PLAYING", 1], 2, 123, 0.5, 123, SceneOut). ---> Se modifica puntaje y estado de juego, ya que terminamos de eliminar al equipo enemigo
+
+
+-------updateScene-------
+updateScene([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [3, 2, 0, 0.5], 1, "PLAYING", 0], 123, SceneOut). ----> Retorna escenario nuevo, ya que cambia Id y se modifica tiempo de viaje. Debido a la representacion, como se cuenta con la posicion inicial, es suficiente con manipular el instante T.
+updateScene([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [3, 2, 0.15, 0.5], 1, "PLAYING", 0], 123, SceneOut). ---> Retorna escenario nuevo, donde se intersecto proyectil con equipo enemigo y se modifica vida, estado y puntaje
+updateScene([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [3, 2, 2, 0.5], 1, "PLAYING", 0], 123, SceneOut). ----> Como el tiempo al que se quiere evaluar es mayor al tiempo maximo de vuelo, retorna el escenario inicial
+
+
+------play------
+play([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0], 2, 1, 1231, 0.4, 123123, SceneOut). ---> Retorna escenario con las siguientes acciones realizadas: mover, disparar, recibir disparo de vuelta y modificar declaradores de estado
+play([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 1], [9, 2, 1]], [], 1, "PLAYING", 0], 2, 1, 1231, 1, 123123, SceneOut). ---> Retorna un escenario donde hemos fallado el disparo y el equipo enemigo acerto.
+play([1, 5, 10, [[1, 2, 1], [2, 2, 1], [3, 2, 1]], [[8, 2, 0], [9, 2, 1]], [], 1, "PLAYING", 0], 2, 1, 1231, 0.5, 123123, SceneOut). ---> Retorna escenario en donde hemos terminado de eliminar al equipo enemigo y se cambia el estado a VICTORY
+
+
+Es importante notar que debido a la naturaleza aleatoria de alguna de estos predicados, puede ser necesario evaluarla en varias ocasiones para obtener el resultado aqui descrito
+*/
